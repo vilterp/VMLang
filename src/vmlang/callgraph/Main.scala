f1635a71d5e3be4db9057f9da199306a7fe77b30
Pete Vilter <bonasaurus1@gmail.com>
2010-01-11 21:11:24 -0500

    added call graph visualizer

---
+++ src/vmlang/callgraph/Main.scala
@@ -0,0 +1,37 @@
+package vmlang.callgraph
+
+import vmlang.common.optparser._
+
+import vmlang.compiler._
+import vmlang.compiler.typecheck._
+import vmlang.compiler.ast._
+
+object Main extends OptParser {
+  
+  def numArgs(n:Int) = n == 1
+  val argErrorMsg = "supply 1 file to show callgraph for"
+  val knownFlags = List()
+  val defaultOpts = Map[String,String]()
+  val help = "usage: vmlcg <file to show call graph for>"
+  
+  def run(args:List[String], flags:List[String], opts:Map[String,String]):Unit =
+      try {
+        val callTuples = extractCalls(TypeCheck(Parse(loadFile(args.head))).defs) flatMap {
+                        case (n, cs) => cs map { c => (n, c) } }
+        callTuples foreach { case (caller, callee) => println(caller + "\t" + callee) }
+      } catch {
+        case e:CompilerError => println(e.repr)
+      }
+  
+  def extractCalls(defs:Map[String,Def]):Map[String,List[String]] =
+      Map() ++ (defs map { case (n,d) => (n, calls(d.body) filter {
+                                    c => !d.params.exists { _.name == c } }) })
+  
+  def calls(e:Expr):List[String] =
+      e match {
+        case a:Atom => Nil
+        case IfExpr(c, i, e) => calls(c) ::: calls(i) ::: calls(e)
+        case Call(name, args) => name :: (args flatMap (calls _))
+      }
+  
+}
